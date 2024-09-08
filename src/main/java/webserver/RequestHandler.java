e1a4fcb11d6b86a5766506c73244fc73b8414aff
Wonyoung Kang <onezerokang@gmail.com>
2024-09-08 17:47:22 +0900

    Chap3 요구사항 2 - GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,11 +3,16 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -25,6 +30,7 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             final List<String> requestHeader = parseRequestHeader(in);
             final String url = requestHeader.get(0).split(" ")[1];
+
             byte[] body = getResponseBodyFromUrl(url);
 
             DataOutputStream dos = new DataOutputStream(out);
@@ -71,10 +77,26 @@ public class RequestHandler extends Thread {
     }
 
     private byte[] getResponseBodyFromUrl(String url) throws IOException {
-        if ("/index.html".equals(url)) {
-            return Files.readAllBytes(new File("./webapp" + url).toPath());
-        } else {
-            return "Hello World".getBytes();
+        final int index = url.indexOf("?");
+        final String requestPath = index == -1 ? url : url.substring(0, index);
+        final Map<String, String> queryString = index == -1 ?
+                new HashMap<>() :
+                HttpRequestUtils.parseQueryString(url.substring(index + 1));
+
+        final Path path = new File("./webapp" + url).toPath();
+        if (Files.exists(path)) {
+            return Files.readAllBytes(path);
         }
+
+        if (requestPath.equals("/user/create")) {
+            signup(queryString.get("userId"), queryString.get("password"), queryString.get("name"), queryString.get("email"));
+        }
+
+        return "Hello, World!".getBytes();
+    }
+
+    private void signup(String userId, String password, String name, String email) {
+        final User user = new User(userId, password, name, email);
+        log.info("Signup successful: User: {}", user);
     }
 }
